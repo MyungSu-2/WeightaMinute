34306ebc33f49fabb1f145827e8239f8ba1d853a
Kevin <choims1379@gmail.com>
2025-05-19 14:33:28 +0900

    Project Configuration
    created project and classified files

---
+++ WeightaMinute/WeightaMinute/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeightaMinute
+//
+//  Created by 최명돌 on 5/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeightaMinute/WeightaMinute/Models/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  WeightaMinute
+//
+//  Created by 최명돌 on 5/19/25.
+//
+
+import UIKit
